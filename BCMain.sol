c8b933cb9a0312db1e87ab3512ca0d110f65ab74
zillerium <sales@merrows.co.uk>
2018-04-30 21:41:19 +0100

    add structs

--- BCMain.sol
+++ BCMain.sol
@@ -1,27 +1,39 @@
 pragma solidity ^0.4.18;
 
-// this contract stores the inventory strings, the hash of the string, and the keys to storage areas
-// to add - events for updates, sha to check passed sha (if an error reject),
 
 contract BCMain {
 
-
-
-    struct JourneyDetail {
+    struct  VehicleDetail {
         bytes32 PlateNumber  ;
         bytes32 ImageLicensePlateHash ;
+    }
+
+    struct LocationDetail {
         bytes32 GPSPos;
         bytes32 GPSTime;
+    }
+
+    struct TariffDetail {
         uint Tariff;
+    }
+
+    struct PaymentDetail {
         bytes32 ShipperAccountHash;
         bytes32 CustomsAccountHash;
         uint PaymentAmount;
+    }
+
+    struct GoodsDetail {
         bytes32 GoodsDesc;
         uint GoodsValue;
         bytes32 GoodsCurrency;
     }
 
-    mapping (bytes32 => JourneyDetail) public JourneyDetails;
+    mapping (bytes32 => VehicleDetail) public VehicleDetails;
+    mapping (bytes32 => LocationDetail) public LocationDetails;
+    mapping (bytes32 => PaymentDetail) public PaymentDetails;
+    mapping (bytes32 => TariffDetail) public TariffDetails;
+    mapping (bytes32 => GoodsDetail) public GoodsDetails;
     bytes32[] public journeyAccounts;
 
 // test
@@ -30,68 +42,36 @@ function addCompany (bytes32 companyID) public {
   journeyAccounts.push(companyID);
 }
 
+function addVehicleDetail (bytes32 companyID, bytes32 _PlateNumber, bytes32 _ImageLicensePlateHash) public {
+    VehicleDetails[companyID].PlateNumber=_PlateNumber;
+    VehicleDetails[companyID].ImageLicensePlateHash=_ImageLicensePlateHash;
+}
 
+function addLocationDetail (bytes32 companyID, bytes32 _GPSPos, bytes32 _GPSTime) public {
+    LocationDetails[companyID].GPSPos=_GPSPos;
+    LocationDetails[companyID].GPSTime=_GPSTime;
+}
 
-    function addJourneyDetail (
-       bytes32 PlateNumber,
-       bytes32 ImageLicensePlateHash,
-       bytes32 GPSPos,
-       bytes32 GPSTime,
-       uint Tariff,
-       bytes32 ShipperAccountHash,
-       bytes32 CustomsAccountHash,
-       uint PaymentAmount,
-       bytes32 GoodsDesc,
-       uint GoodsValue,
-       bytes32 GoodsCurrency,
-       bytes32 companyID
-       ) public {
-        //    JourneyDetail memory jd =   JourneyDetail ({PlateNumber: PlateNumber,
-        //    ImageLicensePlateHash: ImageLicensePlateHash,  GPSPos: GPSPos, GPSTime: GPSTime,Tariff: Tariff,
-        //    ShipperAccountHash: ShipperAccountHash, CustomsAccountHash: CustomsAccountHash,
-        //    PaymentAmount: PaymentAmount, GoodsDesc: GoodsDesc, GoodsValue: GoodsValue, GoodsCurrency: GoodsCurrency});
-        //     JourneyDetails.push(jd);
+function addTariffDetail (bytes32 companyID, uint _Tariff) public {
+    TariffDetails[companyID].Tariff=_Tariff;
+}
 
-
-JourneyDetails[companyID].PlateNumber=PlateNumber;
-JourneyDetails[companyID].ImageLicensePlateHash=ImageLicensePlateHash;
-JourneyDetails[companyID].GPSPos=GPSPos;
-JourneyDetails[companyID].GPSTime=GPSTime;
-JourneyDetails[companyID].Tariff=Tariff;
-JourneyDetails[companyID].ShipperAccountHash=ShipperAccountHash;
-JourneyDetails[companyID].CustomsAccountHash=CustomsAccountHash;
-JourneyDetails[companyID].PaymentAmount=PaymentAmount;
-JourneyDetails[companyID].GoodsDesc=GoodsDesc;
-JourneyDetails[companyID].GoodsValue=GoodsValue;
-JourneyDetails[companyID].GoodsCurrency=GoodsCurrency;
+function addPaymentDetail (bytes32 companyID, bytes32 ShipperAccountHash, bytes32 CustomsAccountHash, uint PaymentAmount) public {
+    PaymentDetails[companyID].ShipperAccountHash=ShipperAccountHash;
+    PaymentDetails[companyID].CustomsAccountHash=CustomsAccountHash;
+    PaymentDetails[companyID].PaymentAmount=PaymentAmount;
+}
+function addGoodsDetail (bytes32 companyID, bytes32 GoodsDesc, uint GoodsValue, bytes32 GoodsCurrency)  public {
+    GoodsDetails[companyID].GoodsDesc=GoodsDesc;
+    GoodsDetails[companyID].GoodsValue=GoodsValue;
+    GoodsDetails[companyID].GoodsCurrency=GoodsCurrency;
+}
 
 
 
-
-      //       JourneyDetail thisJourney = JourneyDetails[companyID];
-
-      //            thisJourney.PlateNumber = PlateNumber;
-      //            thisJourney.ImageLicensePlateHash = ImageLicensePlateHash;
-      //            thisJourney.GPSPos = GPSPos;
-      //            thisJourney.GPSTime = GPSTime;
-      //            thisJourney.Tariff = Tariff;
-      //            thisJourney.ShipperAccountHash = ShipperAccountHash;
-      //            thisJourney.CustomsAccountHash = CustomsAccountHash;
-      //      thisJourney.PaymentAmount = PaymentAmount;
-      //      thisJourney.GoodsDesc = GoodsDesc;
-      //      thisJourney.GoodsValue = GoodsValue;
-      //      thisJourney.GoodsCurrency = GoodsCurrency;
-      //journeyAccounts.push(companyID);
-
-
-       }
-
-    function addTariff(bytes32 CompanyAddress, uint Tariff) public {
-         JourneyDetails[CompanyAddress].Tariff = Tariff;
-    }
 
     function readJourneyDetail (bytes32 CompanyAddress) view  public returns (bytes32) {
-        return JourneyDetails[CompanyAddress].PlateNumber;
+        return VehicleDetails[CompanyAddress].PlateNumber;
     }
 
     function () payable public {
